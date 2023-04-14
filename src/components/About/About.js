9f8f236e619918214620c0832682d648f55454f7
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2023-04-14 16:02:38 -0400

    responsiveness added to all pages

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,15 +6,19 @@ function About() {
   return (
     <div className='About'>
       <h1>About SFPOPOS</h1>
-      <div className='About-image'>
-        <img src={californiaImage} alt="101 California"/>
+      <div className='About-content'>
+        <div className='About-image'>
+          <img src={californiaImage} alt="101 California"/>
+        </div>
+        <div className='About-copy'>
+          <p>POPOS are publicly accessible spaces in
+            forms of plazas, terraces, atriums, small 
+            parks, and even snippets which are provided
+            and maintained by private developers. In San
+            Francisco, POPOS mostly appear in the Downtown
+            office district area.</p>
+        </div>
       </div>
-      <p>POPOS are publicly accessible spaces in
-        forms of plazas, terraces, atriums, small 
-        parks, and even snippets which are provided
-        and maintained by private developers. In San
-        Francisco, POPOS mostly appear in the Downtown
-        office district area.</p>
     </div>
   )
 }
